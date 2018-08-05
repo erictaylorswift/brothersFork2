136f6601adda74572fefad1e744a8e4902946ec1
Max Dubinovsky <m.dubinovsky@arcane.ws>
2018-08-05 18:20:21 -0400

    data injecting

--- client/components/charts/statslist.js
+++ client/components/charts/statslist.js
@@ -8,35 +8,16 @@ class List extends Component {
       stats: [],
     };
   }
-  compenentWillMount() {
-
-    fetch('https://api.fortnitetracker.com/v1/profile/pc/lonefreak', {
-      method: "GET",
-      mode: "no-cors",
-      credentials: "include",
-      headers: {
-        "TRN-Api-Key": "582365b9-87d8-4c86-af13-a4c0caaec1b1"
-      }
-    }).then(results => {
-        return results.json();
-      }).then(data => {
-        let stats = data.results.map((stats) => {
-          return(
-              <li key={stats.results}>
-                {stats.stats.kd.displayValue}
-              </li>
-          )
-      })
-      this.setState({stats : stats});
-      console.log("state", this.state.stats);
-    })
+  componentDidMount() {
   }
 
   render() {
     const {posts} = this.state;
+    console.log('JSON.stringify(window.reactApp, null, 2)', JSON.stringify(window.reactApp, null, 2));
     return (
       <ul>
-        {this.state.stats}
+        Hello?
+        {JSON.stringify(window.reactApp, null, 2)}
       </ul>
     );
   }
--- server/server.js
+++ server/server.js
@@ -6,5 +6,10 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+
+
+
 app.use('/', router);
+
+
 module.exports=app;
